fc04e40e1e16609f7b85055a08af8d35f2421f13
Mai Essam <100529629+Mai265@users.noreply.github.com>
2023-03-10 00:07:58 +0200

    Create dbops.sh

---
+++ dbops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Function, takes customer name, and prints out the id, name, and email. Returns 0 if found, otherwise return 1
+function queryCustomer {
+        local CUSTNAME=${1}
+        LINE=$(grep -w "${CUSTNAME}" customers.db)
+        [ -z ${LINE} ] && printErrorMsg "Sorry, ${CUSTNAME} is not found" && return 7
+        echo "Information for the customer"
+        echo -e "\t ${LINE}"
+        return 0
+}
+exit 0
